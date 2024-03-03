0235bf15fc6cd020e721578996c17c22aaa42780
Murilo Baptista <muboeira@gmail.com>
2024-03-03 13:03:03 -0300

    feat: add custom plugins

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -34,6 +34,7 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
